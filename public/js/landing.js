f5ccb58baa294cecd7938de4c79746fcc34f8827
Breno S. Beletato <breno-beletato@hotmail.com>
2026-03-16 00:31:12 -0300

    feat: redesign da landing page com header fixo e ajustes mobile

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,10 +1,42 @@
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+(function () {
+  var burger = document.querySelector(".burger");
+  var navbar = document.querySelector(".navbar");
+
+  if (burger) {
+    var menu = document.querySelector("#" + burger.dataset.target);
+    var navLinks = menu ? menu.querySelectorAll(".navbar-item") : [];
+
+    burger.addEventListener("click", function () {
+      var isActive = burger.classList.toggle("is-active");
+      if (menu) {
+        menu.classList.toggle("is-active");
+      }
+      burger.setAttribute("aria-expanded", isActive ? "true" : "false");
     });
+
+    navLinks.forEach(function (link) {
+      link.addEventListener("click", function () {
+        burger.classList.remove("is-active");
+        burger.setAttribute("aria-expanded", "false");
+        if (menu) {
+          menu.classList.remove("is-active");
+        }
+      });
+    });
+  }
+
+  function syncNavbarState() {
+    if (!navbar) {
+      return;
+    }
+
+    if (window.scrollY > 16) {
+      navbar.classList.add("is-scrolled");
+    } else {
+      navbar.classList.remove("is-scrolled");
+    }
+  }
+
+  syncNavbarState();
+  window.addEventListener("scroll", syncNavbarState, { passive: true });
 })();
